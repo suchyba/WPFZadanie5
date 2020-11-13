22998ade4830bba26405c2bf4a8e0e17620fe5b1
Bartłomiej Suchecki <suchyba.bq@gmail.com>
2020-11-13 01:39:46 +0100

    Dodaj pliki projektów.

---
+++ WPFZadanie5/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFZadanie5
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Collection<User> Users { get; } = new ObservableCollection<User>();
+        private List<string> regions;
+        public MainWindow()
+        {
+            InitializeComponent();
+            regions = new List<string>{
+                "Białystok", "Warszawa", "Kraków", "Opole", "Wrocław", "Małopolska", "Ślónsk"
+            };
+            RegionComboBox.ItemsSource = regions;
+            UsersListView.ItemsSource = Users;
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (UsersListView.SelectedItems != null)
+                Users.RemoveAt(UsersListView.SelectedIndex);
+        }
+
+        private void UsersListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Users.Add(new User { Name = "brak", Surname = "brak" });
+            UsersListView.SelectedIndex = Users.Count - 1;
+            NameTextBox.SelectAll();
+            NameTextBox.Focus();
+        }
+    }
+}
---
+++ WPFZadanie5/User.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFZadanie5
+{
+    public class User : INotifyPropertyChanged
+    {
+
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                onPropertyChanged("Display");
+            }
+        }
+        private string surname;
+        public string Surname
+        {
+            get => surname;
+            set
+            {
+                surname = value;
+                onPropertyChanged("Display");
+            }
+        }
+        private string email;
+        public string Email
+        {
+            get => email;
+            set
+            {
+                email = value;
+                onPropertyChanged("Display");
+            }
+        }
+        public decimal? Value { get; set; } = null;
+        public string Region { get; set; } = null;
+        public int? Level { get; set; } = null;
+
+        public string Display => $"{Name} {Surname} ({Email ?? "brak"})";
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public User()
+        {
+            Name = null;
+        }
+
+        public void onPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
